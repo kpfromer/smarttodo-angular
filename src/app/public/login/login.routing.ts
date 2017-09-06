e95d1659edb0c5e20fc4613a348ee5f69b234dbb
Kyle <kpfromer2@gmail.com>
2017-09-06 09:29:21 -0600

    Update LoginRoutingModule to use NotLoggedIn
    The login route is know protected from already logged in users.

--- src/app/public/login/login.routing.ts
+++ src/app/public/login/login.routing.ts
@@ -1,16 +1,19 @@
 import {NgModule} from '@angular/core';
 import {RouterModule} from '@angular/router';
 import {LoginComponent} from './login.component';
+import {NotLoggedIn} from '../shared/not-logged-in.service';
 
 @NgModule({
   imports: [
     RouterModule.forChild([
       {
         path: '',
+        canActivate: [NotLoggedIn],
         component: LoginComponent
       }
     ])
   ],
+  providers: [NotLoggedIn],
   exports: [
     RouterModule
   ]
