d67a69e4bed054783ac06f366a0ca950f6931518
Kyle <kpfromer2@gmail.com>
2017-09-06 09:27:39 -0600

    Fix AuthHttp no valid token
    Fixed AuthHttp using sessionStorage, when it should be using localStorage.

--- src/app/shared/auth/auth.module.ts
+++ src/app/shared/auth/auth.module.ts
@@ -5,7 +5,7 @@ import {Http, RequestOptions} from '@angular/http';
 export function authHttpServiceFactory(http: Http, options: RequestOptions) {
   return new AuthHttp(new AuthConfig({
     tokenName: 'id_token',
-    tokenGetter: (() => sessionStorage.getItem('id_token'))
+    tokenGetter: (() => localStorage.getItem('id_token'))
   }), http, options);
 }
 
