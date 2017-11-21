10727e35563c76267930cb239ad9b88356bd6774
Kyle <kpfromer2@gmail.com>
2017-11-21 16:27:26 -0700

    Update AppModule for Angular5
    Uses JwtModule now instead of old AuthModule.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,10 @@ import {NgModule} from '@angular/core';
 import {AppComponent} from './app.component';
 import {AppRoutingModule} from './app.routing';
 import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
-import {AuthModule} from './shared/auth/auth.module';
-import {fakeBackendFactory} from './helpers/fake.backend';
-import {MockBackend} from '@angular/http/testing';
-import {BaseRequestOptions, Http, HttpModule, XHRBackend} from '@angular/http';
-import {environment} from '../environments/environment';
 import {AuthService} from './shared/auth.service';
-import {realBackendFactory} from './helpers/real.backend';
-import {LocalStorageService, WebStorageModule} from 'ngx-store';
+import {WebStorageModule} from 'ngx-store';
+import {JwtModule} from '@auth0/angular-jwt';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -20,25 +16,18 @@ import {LocalStorageService, WebStorageModule} from 'ngx-store';
   ],
   imports: [
     BrowserModule,
-    AuthModule,
-    HttpModule,
+    HttpClientModule,
+    JwtModule.forRoot({
+      config: {
+        tokenGetter: () => {
+          return localStorage.getItem('id_token');
+        }
+      }
+    }),
     WebStorageModule,
     AppRoutingModule
   ],
   providers: [
-
-    environment.production ? {
-      provide: Http,
-      useFactory: realBackendFactory,
-      deps: [XHRBackend, BaseRequestOptions]
-    } : {
-      provide: Http,
-      useFactory: fakeBackendFactory,
-      deps: [MockBackend, BaseRequestOptions, LocalStorageService]
-    },
-    XHRBackend,
-    MockBackend,
-    BaseRequestOptions,
     AuthService
   ],
   bootstrap: [AppComponent]
