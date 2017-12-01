fa6c1150b0755240b0f053863e35e9468f545fd1
Kyle <kpfromer2@gmail.com>
2017-11-30 17:52:32 -0700

    Update FlashMessageComponent to seem like material design
    Added icon and colored text and updated spec. Also switched to scss to get theme colors from material.

--- src/app/shared/flash-message/flash-message.module.ts
+++ src/app/shared/flash-message/flash-message.module.ts
@@ -1,9 +1,13 @@
 import {NgModule} from '@angular/core';
 import {FlashMessageComponent} from './flash-message.component';
 import {CommonModule} from '@angular/common';
+import {MatIconModule} from '@angular/material';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatIconModule
+  ],
   declarations: [FlashMessageComponent],
   exports: [FlashMessageComponent]
 })
