ec81328b93aff59374c486e697322029b43be313
Kyle <kpfromer2@gmail.com>
2017-12-04 20:53:09 -0700

    Update app to use dark theme and have basic material structure

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,34 +1,73 @@
-import {async, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {AppComponent} from './app.component';
 import {RouterTestingModule} from '@angular/router/testing';
+import {By} from '@angular/platform-browser';
+import {FlexLayoutModule} from '@angular/flex-layout';
+import {MatButtonModule, MatIconModule, MatMenuModule, MatSidenavModule, MatSnackBarModule, MatToolbarModule} from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {OverlayContainer} from '@angular/cdk/overlay';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [
+        RouterTestingModule,
+        BrowserAnimationsModule,
+        FlexLayoutModule,
+        MatToolbarModule,
+        MatButtonModule,
+        MatSidenavModule,
+        MatIconModule,
+        MatMenuModule,
+        MatSnackBarModule
+      ],
       declarations: [
         AppComponent
       ],
     }).compileComponents();
   }));
 
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+  });
+
   it('should create the app', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   }));
 
-  it(`should have as title 'app'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('app');
-  }));
+  it('should use dark theme', () => {
+    component.isDark = true;
 
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to app!');
-  }));
+
+    const mainElement = fixture.debugElement.query(By.css('.app-frame'));
+
+    expect(mainElement.classes['dark-theme']).toBe(true);
+  });
+
+  describe('isDark', () => {
+    let overlayContainer: OverlayContainer;
+
+    beforeEach(() => {
+      overlayContainer = TestBed.get(OverlayContainer);
+    });
+
+    it('should add dark theme class to overlays', () => {
+      component.isDark = true;
+
+      expect(overlayContainer.getContainerElement().classList.contains('dark-theme')).toBe(true);
+    });
+
+    it('should remove dark theme class from overlays', () => {
+      component.isDark = true;
+      component.isDark = false;
+
+      expect(overlayContainer.getContainerElement().classList.contains('dark-theme')).toBe(false);
+    });
+  });
 });
