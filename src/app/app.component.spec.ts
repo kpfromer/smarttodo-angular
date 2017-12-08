fdef2293523797fb67728681d622c89b8937edf2
Kyle <kpfromer2@gmail.com>
2017-12-08 09:23:35 -0700

    Update app component to have functioning logout button

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,39 +1,73 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {async, ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
 
 import {AppComponent} from './app.component';
 import {RouterTestingModule} from '@angular/router/testing';
 import {By} from '@angular/platform-browser';
 import {FlexLayoutModule} from '@angular/flex-layout';
-import {MatButtonModule, MatIconModule, MatMenuModule, MatSidenavModule, MatSnackBarModule, MatToolbarModule} from '@angular/material';
+import {MatButtonModule, MatIconModule, MatMenuModule, MatSidenavModule, MatSnackBar, MatToolbarModule} from '@angular/material';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {OverlayContainer} from '@angular/cdk/overlay';
+import {AuthService} from './shared/auth.service';
+import {Router} from '@angular/router';
+import {Component} from '@angular/core';
+import {of} from 'rxjs/observable/of';
+import {Subject} from 'rxjs/Subject';
+
+@Component({
+  template: ''
+})
+class DummyComponent {
+}
 
 describe('AppComponent', () => {
   let component: AppComponent;
   let fixture: ComponentFixture<AppComponent>;
 
+  const mockAuthService = {
+    loggedIn: () => {
+    },
+    logout: () => {
+    }
+  };
+
+  const mockSnackbar = {
+    open: () => {
+    }
+  };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule,
+        RouterTestingModule.withRoutes([
+          {path: 'login', component: DummyComponent}
+        ]),
         BrowserAnimationsModule,
         FlexLayoutModule,
         MatToolbarModule,
         MatButtonModule,
         MatSidenavModule,
         MatIconModule,
-        MatMenuModule,
-        MatSnackBarModule
+        MatMenuModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        DummyComponent
       ],
+      providers: [
+        {provide: AuthService, useValue: mockAuthService},
+        {provide: MatSnackBar, useValue: mockSnackbar}
+      ]
     }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(AppComponent);
     component = fixture.componentInstance;
+  });
+
+  beforeEach(() => {
+    component.isDark = false;
+    fixture.detectChanges();
   });
 
   it('should create the app', async(() => {
@@ -69,5 +103,73 @@ describe('AppComponent', () => {
 
       expect(overlayContainer.getContainerElement().classList.contains('dark-theme')).toBe(false);
     });
+  });
+
+  describe('logout', () => {
+    let authService: AuthService;
+    let router: Router;
+    let snackbar: MatSnackBar;
+
+    beforeEach(() => {
+      authService = TestBed.get(AuthService);
+      router = TestBed.get(Router);
+      snackbar = TestBed.get(MatSnackBar);
+    });
+
+    it('should log out', () => {
+      spyOn(authService, 'logout').and.returnValue(true);
+
+      component.logout();
+
+      expect(authService.logout).toHaveBeenCalled();
+    });
+
+    it('should route to /home', () => {
+      spyOn(router, 'navigateByUrl');
+      spyOn(authService, 'logout').and.returnValue(true);
+
+      component.logout();
+
+      expect(router.navigateByUrl).toHaveBeenCalledWith('/home');
+    });
+
+    it('should display a snackbar message', () => {
+      spyOn(snackbar, 'open');
+      spyOn(authService, 'logout').and.returnValue(true);
+
+      component.logout();
+
+      expect(snackbar.open).toHaveBeenCalledWith('Successfully Logged Out', 'DISMISS', {
+        duration: 3500
+      });
+    });
+
+    it('should display a snackbar message if it fails to logout', () => {
+      spyOn(snackbar, 'open').and.returnValue({
+        onAction: () => of()
+      });
+      spyOn(authService, 'logout').and.returnValue(false);
+
+      component.logout();
+
+      expect(snackbar.open).toHaveBeenCalledWith('Failed to Logout', 'RETRY', {
+        duration: 3500
+      });
+    });
+
+    it('should retry logging out if the user triggers snackbar', fakeAsync(() => {
+      spyOn(authService, 'logout').and.returnValue(false);
+      const action = new Subject<any[]>();
+      spyOn(snackbar, 'open').and.returnValue({
+        onAction: () => action.asObservable()
+      });
+      spyOn(component, 'logout').and.callThrough();
+
+      component.logout();
+      action.next();
+      tick();
+
+      expect(component.logout).toHaveBeenCalledTimes(2);
+    }));
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import {Component} from '@angular/core';
 import {OverlayContainer} from '@angular/cdk/overlay';
+import {AuthService} from './shared/auth.service';
+import {MatSnackBar} from '@angular/material';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -9,13 +12,32 @@ import {OverlayContainer} from '@angular/cdk/overlay';
 export class AppComponent {
   title = 'app';
 
-  constructor(private overlayContainer: OverlayContainer) {
+  constructor(private overlayContainer: OverlayContainer,
+              public authSerivce: AuthService,
+              private snackBar: MatSnackBar,
+              private router: Router) {
   }
 
   private _isDark = false;
 
   get isDark() {
     return this._isDark;
+  }
+
+  logout() {
+    if (this.authSerivce.logout()) {
+      this.router.navigateByUrl('/home');
+      this.snackBar.open('Successfully Logged Out', 'DISMISS', {
+        duration: 3500
+      });
+    } else {
+      const snackbar = this.snackBar.open('Failed to Logout', 'RETRY', {
+        duration: 3500
+      });
+      snackbar.onAction().subscribe(() => {
+        this.logout();
+      });
+    }
   }
 
   set isDark(value: boolean) {
