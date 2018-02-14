92a573bca31904194cc5625d713eedbb0ce5aae6
Kyle <kpfromer2@gmail.com>
2018-02-14 09:12:46 -0700

    Add basic description and filler to home page

--- src/app/public/home/home.module.ts
+++ src/app/public/home/home.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HomeComponent} from './home.component';
 import {HomeRoutingModule} from './home.routing';
+import {MatToolbarModule} from '@angular/material';
 
 @NgModule({
   imports: [
     CommonModule,
+    MatToolbarModule,
     HomeRoutingModule
   ],
   declarations: [HomeComponent]
