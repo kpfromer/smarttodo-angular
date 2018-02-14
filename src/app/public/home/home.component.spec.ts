432d522225b9a2b250f1f783da4624931ec63a5f
Kyle <kpfromer2@gmail.com>
2018-02-14 09:21:27 -0700

    Fix HomeComponent should be created test error

--- src/app/public/home/home.component.spec.ts
+++ src/app/public/home/home.component.spec.ts
@@ -1,6 +1,7 @@
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {HomeComponent} from './home.component';
+import {MatToolbarModule} from '@angular/material';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -8,6 +9,7 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
+      imports: [MatToolbarModule],
       declarations: [HomeComponent]
     })
       .compileComponents();
