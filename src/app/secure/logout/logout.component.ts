67d8891ea1b66e512b6cb92b3f7942e731d4a0f9
Kyle <kpfromer2@gmail.com>
2018-02-14 14:35:47 -0700

    Add Snackbar Message for LogoutComponent

--- src/app/secure/logout/logout.component.ts
+++ src/app/secure/logout/logout.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
+import {MatSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-logout',
@@ -8,16 +9,18 @@ import {Router} from '@angular/router';
 })
 export class LogoutComponent implements OnInit {
 
-
-  constructor(private router: Router) { }
+  constructor(private router: Router, private snackBar: MatSnackBar) {
+  }
 
   logOut() {
     localStorage.removeItem('id_token');
     this.router.navigateByUrl('/home');
+    this.snackBar.open('Successfully Logged Out', 'DISMISS', {
+      duration: 3500
+    });
   }
 
   ngOnInit() {
     this.logOut();
   }
-
 }
